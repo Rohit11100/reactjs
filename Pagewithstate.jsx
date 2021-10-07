95cd0db18dca4fe8a94c34ddb316fd0a4910c7dd
Rohit11100 <90681714+Rohit11100@users.noreply.github.com>
2021-10-07 16:59:12 +0530

    Create Pagewithstate.jsx

---
+++ Pagewithstate.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export class App extends Component {
+  state = {
+    posts: [],
+    loading: false,
+    currentPage: 1,
+    postsPerPage: 5
+  };
+
+  componentDidMount() {
+    const getPosts = async () => {
+      this.setState({ loading: true });
+      const results = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      this.setState({ posts: results.data });
+      this.setState({ loading: false });
+    };
+
+    getPosts();
+  }
+
+  render() {
+    const { currentPage, postsPerPage, posts, loading } = this.state;
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = pageNum => this.setState({ currentPage: pageNum });
+
+    const nextPage = () => this.setState({ currentPage: currentPage + 1 });
+
+    const prevPage = () => this.setState({ currentPage: currentPage - 1 });
+
+    return (
+      <div className="container">
+        <h1 className="my-5 text-primary text-center">React Pagination</h1>
+        <Posts posts={currentPosts} loading={loading} />
+        <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} nextPage={nextPage} prevPage={prevPage} />
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+
+function Posts(){
+   const { posts, loading } = this.props;
+
+        if (loading) {
+            return <h2>Loading...</h2>
+        }
+  
+  return (
+            <div>
+                {posts.map(post => (
+                    <div key={post.id} className="alert alert-primary">
+                        <h4 className="alert-heading">{post.title}</h4>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        )
+  
+}
+
+
+function (){
+ const { postsPerPage, totalPosts, paginate, nextPage, prevPage } = this.props;
+
+        const pageNumbers = [];
+
+        for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+            pageNumbers.push(i);  
+   }
+
+        return (
+            <nav>
+                <ul className="pagination justify-content-center">
+                    <li className="page-item">
+                        <a className="page-link" href="#" onClick={() => prevPage()}>Previous</a>
+                    </li>
+                    {pageNumbers.map(num => (
+                        <li className="page-item" key={num}>
+                            <a onClick={() => paginate(num)} href="#" className="page-link">{num}</a>
+                        </li>
+                    ))}
+                    <li className="page-item">
+                        <a className="page-link" href="#" onClick={() => nextPage()}>Next</a>
+                    </li>
+                </ul>
+            </nav>
+        )
+
+
+
+}
+
+
+
+
+
+
+
+
+
